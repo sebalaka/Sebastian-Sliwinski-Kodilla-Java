fdd3e9838dfe07404ff9fd6226e7fbdeb68bec3b
sebalaka <sebalaka@gmail.com>
2020-01-05 13:38:15 +0100

    zadanie 7.3 koniec

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,31 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
 
 public class StreamMain {
     public static void main(String[] args) {
-        BookDirectory theBookDirectory = new BookDirectory();
-        String theResultStringOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
 
-        System.out.println(theResultStringOfBooks);
+        Forum theForumUser = new Forum();
+        theForumUser.getForumUsers().stream()
+                .filter(forumuser -> forumuser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getBirthDate().getYear() <= 1999)
+                .filter(forumUser -> forumUser.getPostNumber() >= 1)
+                .forEach(System.out::println);
+
+
+        Map<Integer, ForumUser> par = theForumUser.getForumUsers().stream()
+                .collect(Collectors.toMap(ForumUser::getId, fu -> fu));
+
+        System.out.println("# elements: " + par.size());
+        par.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -10,9 +10,9 @@ public class Forum {
     public Forum(){
         forumUsers.add(new ForumUser(1,"Marcin",'M',2002,12,1,131));
         forumUsers.add(new ForumUser(2,"Sylwia",'F',1990,3,22,1211));
-        forumUsers.add(new ForumUser(3,"Maciej",'M',1998,4,14,1211));
-        forumUsers.add(new ForumUser(4,"Dariusz",'M',1981,7,2,1211));
-        forumUsers.add(new ForumUser(5,"Ewa",'F',2000,3,12,1211));
+        forumUsers.add(new ForumUser(3,"Maciej",'M',1998,4,14,10));
+        forumUsers.add(new ForumUser(4,"Dariusz",'M',1981,7,2,137));
+        forumUsers.add(new ForumUser(5,"Ewa",'F',2000,3,12,357));
     }
 
     public List<ForumUser> getForumUsers() {
